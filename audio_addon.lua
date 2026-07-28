ee8fba0d526b9b46f14ea6af5a54c4de19a87f65
sdurutr436 <sdurutr436@g.educaand.es>
2026-07-29 01:03:40 +0200

    feat: añadidos a la carpeta correspondiente los audios de uk y wow.

--- audio_addon.lua
+++ audio_addon.lua
@@ -4,47 +4,57 @@ local SOUND_DIR = "Interface\\AddOns\\" .. ADDON_NAME .. "\\Sounds\\"
 -- Un archivo en Sounds\ (.ogg|.mp3|.wav, ver Sounds\FORMATS.md) por entrada.
 -- clave = nombre de archivo CON extension, valor = nombre mostrado en EllesmereUI.
 local CUES = {
+  -- Ultrakill
 
-    -- General, se incluyen audios de Ultrakill.
+    -- Gabriel
+    ["ultrakill\\gab_behold.ogg"] = "Gabriel: Behold",
+    ["ultrakill\\gab_defylight.ogg"] = "Gabriel: Defy The Light",
+    ["ultrakill\\gab_divinejustice.wav"] = "Gabriel: Divine Justice",
+    ["ultrakill\\gab_youneedmorepower.wav"] = "Gabriel: You Need More Power",
 
-    -- Ultrakill Gabriel
+    -- Minos Prime
+    ["ultrakill\\mp_crush.ogg"] = "Minos Prime: Crush",
+    ["ultrakill\\mp_intro_long.wav"] = "Minos Prime: Introduction Long",
+    ["ultrakill\\mp_judgement.ogg"] = "Minos Prime: Judgement",
+    ["ultrakill\\mp_prepare1.ogg"] = "Minos Prime: Prepare 1",
+    ["ultrakill\\mp_prepare2.ogg"] = "Minos Prime: Prepare 2",
+    ["ultrakill\\mp_thyend1.ogg"] = "Minos Prime: Thy End 1",
+    ["ultrakill\\mp_thyend2.ogg"] = "Minos Prime: Thy End 2",
+    ["ultrakill\\mp_useless.wav"] = "Minos Prime: Useless",
 
-    ["Gab_Behold.ogg"] = "Behold!",
-    ["Gab_Taunt1.ogg"] = "You defy the light!",
+    -- Sisyphus Prime
+    ["ultrakill\\sp_begone.wav"] = "Sisyphus Prime: Begone",
+    ["ultrakill\\sp_keepthemcoming.wav"] = "Sisyphus Prime: Keep Them Coming",
+    ["ultrakill\\sp_nicetry.wav"] = "Sisyphus Prime: Nice Try",
+    ["ultrakill\\sp_thiswillhurt.wav"] = "Sisyphus Prime: This Will Hurt",
+    ["ultrakill\\sp_yesthatsit.wav"] = "Sisyphus Prime: Yes That's It",
 
-    -- Ultrakill Minos Prime
+    -- Power
+    ["ultrakill\\pow_glaive.ogg"] = "Power: Glaive",
+    ["ultrakill\\pow_glaiveshot.ogg"] = "Power: Glaive Shot",
+    ["ultrakill\\pow_greatsword.ogg"] = "Power: Greatsword",
+    ["ultrakill\\pow_overhere.ogg"] = "Power: Over Here",
+    ["ultrakill\\pow_spear.ogg"] = "Power: Spear",
+    ["ultrakill\\pow_wrongtarget.ogg"] = "Power: Wrong Target",
 
-    ["Mp_crush.ogg"] = "Crush!",
-    ["Mp_judgement.ogg"] = "Judgement!",
+    -- V1
+    ["ultrakill\\v1_bell.ogg"] = "V1: Bell",
+    ["ultrakill\\v1_chainsaw.ogg"] = "V1: Chainsaw",
+    ["ultrakill\\v1_coinflip.ogg"] = "V1: Coin Flip",
+    ["ultrakill\\v1_hpget.wav"] = "V1: Health Pickup",
+    ["ultrakill\\v1_pump.ogg"] = "V1: Pump Charging",
+    ["ultrakill\\v1_punch.ogg"] = "V1: Punch",
+    ["ultrakill\\v1_tv.ogg"] = "V1: Television Off",
+    ["ultrakill\\v1_whip.ogg"] = "V1: Whiplash",
 
-    -- Audios de WoW
+  -- World Of Warcraft
 
     -- Venel
+    ["wow\\Venel_Crushed.ogg"] = "Venel: Crushed",
+    ["wow\\Venel_bodies_broken.ogg"] = "Venel: Bodies Broken",
 
-    ["Venel_Crushed.ogg"] = "Crushed!",
-    ["Venel_bodies_broken.ogg"] = "Bodies Broken!",
-
-
-    -- Generados con IA
-
-    -- Paladin Female
-    ["Paladin_Judgement.mp3"] = "Paladin_Judgement_Female",
-    ["Paladin_Blade.mp3"] = "Paladin_BladeOfJustice_Female",
-    ["Paladin_WakeOfAshes.mp3"] = "Paladin_WakeOfAshes_Female",
-    ["Paladin_DivineToll.mp3"] = "Paladin_DivineToll_Female",
-    ["Paladin_Rebuke.mp3"] = "Paladin_Rebuke_Female",
-    ["Paladin_"],
-
-    -- Paladin SAM
-    ["Paladin_Judgement_SAM.mp3"] = "Paladin_Judgement_SAM",
-    ["Paladin_Blade_SAM.mp3"] = "Paladin_BladeOfJustice_SAM",
-    ["Paladin_WakeOfAshes_SAM.mp3"] = "Paladin_WakeOfAshes_SAM",
-    ["Paladin_DivineToll_SAM.mp3"] = "Paladin_DivineToll_SAM",
-    ["Paladin_Rebuke_SAM.mp3"] = "Paladin_Rebuke_SAM",
-    
 }
 
--- PLAYER_LOGIN garantiza que todos los addons (incl. EllesmereUI, que trae
 -- LibStub + LibSharedMedia-3.0 embebidos) ya cargaron, sin depender del
 -- orden de carga entre addons.
 local f = CreateFrame("Frame")
